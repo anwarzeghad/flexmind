4058bfc80d9bed774151ac0b22b19a037a4923f9
ZEGHAD Anwar <anwar.zeghad.enp.uspn@gmail.com>
2025-07-28 22:44:52 +0200

    formulaire de contact

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Contact from './pages/Contact';
 import { Scroll } from 'lucide-react'
 import ScrollProgress from './components/ScrollProgress'
 import Faq from './pages/Faq'
-import Formulaire from './pages/formulaire'
+import Formulaire from './pages/Formulaire'
 
 function App() {
   return (
