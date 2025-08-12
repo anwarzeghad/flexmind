40b727a1c713373966499ea919e0a00f042773cb
ZEGHAD Anwar <anwar.zeghad.enp.uspn@gmail.com>
2025-08-12 07:50:17 +0200

    vers le formulaire

--- src/pages/Formulaire.jsx
+++ src/pages/Formulaire.jsx
@@ -87,7 +87,7 @@ const Formulaire = () => {
   };
 
   return (
-    <section className="py-32 px-4 sm:px-6 lg:px-8 bg-gradient-to-b from-white to-[#e9f9f8] dark:from-gray-900 dark:to-gray-800">
+    <section id="formulaire" className="py-32 px-4 sm:px-6 lg:px-8 bg-gradient-to-b from-white to-[#e9f9f8] dark:from-gray-900 dark:to-gray-800">
       <div className="max-w-7xl mx-auto text-center mb-16">
         <motion.h1 
           initial={{ opacity: 0, y: 20 }}
